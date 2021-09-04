93a616dced44a36b6a7ace6cf6d85a559c651f99
Tyler Young <tyoung@enbala.com>
2021-09-03 20:16:17 -0500

    Make List.rotate take a range instead of a start & end index

--- lib/x_util/list.ex
+++ lib/x_util/list.ex
@@ -4,17 +4,42 @@ defmodule XUtil.List do
   """
 
   @doc """
-  Given three indices (start, middle, and last, inclusive), this pulls out the elements
-  in the range [`middle`, `last`] and inserts them at index `start`. This reorders the range
-  [`first`, `last`] such that the item at index `middle` becomes first, and the item at index
-  `middle` - 1 becomes the last.
+  Given a range of list indices, pulls out the elements in that range and inserts them
+  in front of the previous value at the insertion index.
 
-    iex> XUtil.List.rotate([0, 1, 2, 3, 4, 5, 6], 1, 3, 5)
+    iex> XUtil.List.rotate([0, 1, 2, 3, 4, 5, 6], 3..5, 1)
     [0, 3, 4, 5, 1, 2, 6]
-    iex> XUtil.List.rotate([0, 1, 2, 3, 4, 5, 6], 0, 1, 7)
+    iex> XUtil.List.rotate([0, 1, 2, 3, 4, 5, 6], 1..7, 0)
     [1, 2, 3, 4, 5, 6, 0]
+
+    iex> XUtil.List.rotate([0, 1, 2, 3, 4, 5, 6], 1..3, 5)
+    [0, 4, 5, 1, 2, 3, 6]
+    iex> XUtil.List.rotate([0, 1, 2, 3, 4, 5, 6], 2..4, 1)
+    [0, 2, 3, 4, 1, 5, 6]
   """
-  def rotate(enumerable, start, middle, last) do
+  # TODO: Support negative indices/ranges
+  # TODO: Make it clear the semantics of the range are based on the semantics of Enum.slice/2
+  #       and the semantics of "end" on insert_at
+  # TODO: Support ranges with a :step (1.12+)
+  def rotate(enumerable, %Range{first: first, last: last}, insertion_index)
+      when first != insertion_index do
+    if insertion_index < first do
+      rotate_contiguous(enumerable, insertion_index, first, last)
+    else
+      rotate_contiguous(enumerable, first, last + 1, insertion_index)
+    end
+  end
+
+  def rotate(enumerable, _range, _insertion_index) do
+    Enum.to_list(enumerable)
+  end
+
+  # This has the semantics of C++'s std::rotate
+  # Given three indices (start, middle, and last, inclusive), this pulls out the elements
+  # in the range [`middle`, `last`] and inserts them at index `start`. This reorders the range
+  # [`first`, `last`] such that the item at index `middle` becomes first, and the item at index
+  # `middle` - 1 becomes the last.
+  defp rotate_contiguous(enumerable, start, middle, last) do
     {unchanged_start, after_insertion_pt} = Enum.split(enumerable, start)
     # to_rotate contains just the elements in the range [start, last]
     {to_rotate, unchanged_end} = Enum.split(after_insertion_pt, last - start + 1)
@@ -24,37 +49,14 @@ defmodule XUtil.List do
   end
 
   @doc """
-  Pulls out the elements in the range [`range_start_idx`, `range_end_idx`] and inserts them at
-  `insertion_idx`, returning the reassembled enumerable as a list.
-
-    iex> XUtil.List.slide([0, 1, 2, 3, 4, 5, 6], 1, 3, 5)
-    [0, 4, 5, 1, 2, 3, 6]
-    iex> XUtil.List.slide([0, 1, 2, 3, 4, 5, 6], 2, 4, 1)
-    [0, 2, 3, 4, 1, 5, 6]
-  """
-  def slide(enumerable, range_start_idx, range_end_idx, insertion_idx)
-
-  def slide(enumerable, range_start_idx, range_end_idx, insertion_idx)
-      when insertion_idx < range_start_idx do
-    rotate(enumerable, insertion_idx, range_start_idx, range_end_idx)
-  end
-
-  def slide(enumerable, range_start_idx, range_end_idx, insertion_idx)
-      when insertion_idx > range_start_idx do
-    rotate(enumerable, range_start_idx, range_end_idx + 1, insertion_idx)
-  end
-
-  def slide(enumerable, _, _, _), do: Enum.to_list(enumerable)
-
-  @doc """
   Moves a single element from its current index to a target index.
 
-    iex> XUtil.List.slide_one([0, 1, 2, 3, 4, 5, 6], 2, 4)
-    [0, 1, 3, 4, 2, 5, 6]
-    iex> XUtil.List.slide_one([0, 1, 2, 3, 4, 5, 6], 5, 1)
+    iex> XUtil.List.rotate_one([0, 1, 2, 3, 4, 5, 6], 5, 1)
     [0, 5, 1, 2, 3, 4, 6]
+    iex> XUtil.List.rotate_one([0, 1, 2, 3, 4, 5, 6], 2, 4)
+    [0, 1, 3, 4, 2, 5, 6]
   """
-  def slide_one(enumerable, idx_to_move, insertion_idx) do
-    slide(enumerable, idx_to_move, idx_to_move, insertion_idx)
+  def rotate_one(enumerable, idx_to_move, insertion_idx) do
+    rotate(enumerable, idx_to_move..idx_to_move, insertion_idx)
   end
 end
