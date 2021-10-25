d70e7b316f5350dfa21f7080f96d24a819f46d62
Tyler Young <tyoung@enbala.com>
2021-10-24 21:17:03 -0500

    Initial version of rotate implementation for any enumerable

--- lib/x_util/enum.ex
+++ lib/x_util/enum.ex
@@ -27,4 +27,143 @@ defmodule XUtil.Enum do
   def none?(enumerable, predicate) do
     not Enum.any?(enumerable, predicate)
   end
+
+  @doc """
+  Pulls out either a single element (denoted by an integer index) or a contiguous range of
+  the enumerable (given by a range) and inserts it in front of the value previously at the
+  insertion index.
+
+  The semantics of the range to be rotated match the semantics of Enum.slice/2. Specifically,
+  that means:
+
+  - Indices are normalized, meaning that negative indexes will be counted from the end
+      (for example, -1 means the last element of the enumerable). This will result in *two*
+      traversals of your enumerable on types like lists that don't provide a constant-time count.
+  - If the normalized index range's `last` is out of bounds, the range is truncated to the last element.
+  - If the normalized index range's `first` is out of bounds, the selected range for rotation
+      will be empty, so you'll get back your input list.
+  - Decreasing ranges (e.g., the range 5..0//1) also select an empty range to be rotated, so you'll
+      get back your input list.
+  - Ranges with any step but 1 will raise an error.
+
+    # Rotate a single element
+    iex> XUtil.Enum.rotate([0, 1, 2, 3, 4, 5, 6], 5, 1)
+    [0, 5, 1, 2, 3, 4, 6]
+    iex> XUtil.Enum.rotate(0..6, 2, 4)
+    [0, 1, 3, 4, 2, 5, 6]
+
+    # Rotate a range of elements backward
+    iex> XUtil.Enum.rotate([0, 1, 2, 3, 4, 5, 6], 3..5, 1)
+    [0, 3, 4, 5, 1, 2, 6]
+    iex> XUtil.Enum.rotate(0..6, 1..7, 0)
+    [1, 2, 3, 4, 5, 6, 0]
+
+    # Rotate a range of elements forward
+    iex> XUtil.Enum.rotate([0, 1, 2, 3, 4, 5, 6], 1..3, 5)
+    [0, 4, 5, 1, 2, 3, 6]
+    iex> XUtil.Enum.rotate([0, 1, 2, 3, 4, 5, 6], 2..4, 1)
+    [0, 2, 3, 4, 1, 5, 6]
+
+    # Rotate with negative indices (counting from the end)
+    iex> XUtil.Enum.rotate([0, 1, 2, 3, 4, 5, 6], 3..-1//1, 2)
+    [0, 1, 3, 4, 5, 6, 2]
+    iex> XUtil.Enum.rotate([0, 1, 2, 3, 4, 5, 6], -4..-2, 1)
+    [0, 3, 4, 5, 1, 2, 6]
+  """
+  def rotate(enumerable, range_or_single_index, insertion_index)
+
+  def rotate(enumerable, single_index, insertion_index) when is_integer(single_index) do
+    rotate(enumerable, single_index..single_index, insertion_index)
+  end
+
+  def rotate(enumerable, %Range{first: insertion_index}, insertion_index) do
+    Enum.to_list(enumerable)
+  end
+
+  # This matches the behavior of Enum.slice/2
+  def rotate(_, %Range{step: step} = index_range, _insertion_index) when step != 1 do
+    raise ArgumentError,
+          "Enum.rotate/3 does not accept ranges with custom steps, got: #{inspect(index_range)}"
+  end
+
+  # Normalize negative input ranges like Enum.slice/2
+  def rotate(enumerable, first..last, insertion_index) when first < 0 or last < 0 do
+    count = Enum.count(enumerable)
+    normalized_first = if first >= 0, do: first, else: first + count
+    normalized_last = if last >= 0, do: last, else: last + count
+
+    if normalized_first >= 0 and normalized_first < count and normalized_first != insertion_index do
+      normalized_range = normalized_first..normalized_last//1
+      rotate(enumerable, normalized_range, insertion_index)
+    else
+      Enum.to_list(enumerable)
+    end
+  end
+
+  def rotate(_, first..last, insertion_index)
+      when insertion_index > first and insertion_index < last do
+    raise "Insertion index for rotate must be outside the range being moved " <>
+            "(tried to insert #{first}..#{last} at #{insertion_index})"
+  end
+
+  # Guarantees at this point: step size == 1 and first <= last and (insertion_index < first or insertion_index > last)
+  def rotate(enumerable, first..last, insertion_index) do
+    impl = if is_list(enumerable), do: &find_start/4, else: &rotate_any/4
+
+    cond do
+      insertion_index <= first -> impl.(enumerable, insertion_index, first, last)
+      insertion_index > last -> impl.(enumerable, first, last + 1, insertion_index)
+    end
+  end
+
+  # Takes the range from middle..last and moves it to index first
+  defp rotate_any(enumerable, start, middle, last) do
+    {:done, {_size, head, rotate_back, rotate_forward, tail}} =
+      Enumerable.reduce(enumerable, {:cont, {0, [], [], [], []}}, fn item, {index, chunk_1, chunk_2, chunk_3, tail} ->
+        cond do
+          index < start -> {:cont, {index + 1, [item | chunk_1], chunk_2, chunk_3, tail}}
+          index >= start and index < middle -> {:cont, {index + 1, chunk_1, [item | chunk_2], chunk_3, tail}}
+          index >= middle and index <= last -> {:cont, {index + 1, chunk_1, chunk_2, [item | chunk_3], tail}}
+          true -> {:cont, {index + 1, chunk_1, chunk_2, chunk_3, [item | tail]}}
+        end
+      end)
+
+    :lists.reverse(head, :lists.reverse(rotate_forward, :lists.reverse(rotate_back, :lists.reverse(tail))))
+  end
+
+  # If end is after middle, we can use a non-tail recursive to start traverse until we find the start:
+  # This guarantees the list is only copied once (plus one additional copy for the start..middle slice).
+  # A similar approach can be devised if the end is before the start.
+  defp find_start([h | t], start, middle, last)
+       when start > 0 and start <= middle and middle <= last do
+    [h | find_start(t, start - 1, middle - 1, last - 1)]
+  end
+
+  defp find_start(list, 0, middle, last), do: accumulate_start_middle(list, middle, last, [])
+
+  defp accumulate_start_middle([h | t], middle, last, acc) when middle > 0 do
+    accumulate_start_middle(t, middle - 1, last - 1, [h | acc])
+  end
+
+  defp accumulate_start_middle(list, 0, last, start_to_middle) do
+    {rotated_range, tail} = accumulate_middle_last(list, last + 1, [])
+    rotated_range ++ :lists.reverse(start_to_middle, tail)
+  end
+
+  # You asked for a middle index off the end of the list... you get what we've got
+  defp accumulate_start_middle([], _, _, acc) do
+    :lists.reverse(acc)
+  end
+
+  defp accumulate_middle_last([h | t], last, acc) when last > 0 do
+    accumulate_middle_last(t, last - 1, [h | acc])
+  end
+
+  defp accumulate_middle_last(rest, 0, acc) do
+    {:lists.reverse(acc), rest}
+  end
+
+  defp accumulate_middle_last([], _, acc) do
+    {:lists.reverse(acc), []}
+  end
 end
--- test/enum_test.exs
+++ test/enum_test.exs
@@ -1,4 +1,134 @@
 defmodule XUtil.EnumTest do
   use ExUnit.Case, async: true
+  use ExUnitProperties
   doctest XUtil.Enum
+
+  describe "rotate" do
+    property "matches behavior for lists vs. ranges" do
+      # Below 32 elements, the map implementation currently sticks the pairs these in order
+      range = 0..20
+      list = Enum.to_list(range)
+      set = MapSet.new(list)
+
+      check all({range, insertion_point} <- rotation_spec(list)) do
+        rotation = &XUtil.Enum.rotate(&1, range, insertion_point)
+        assert rotation.(list) == rotation.(set)
+      end
+
+      zipped_list = Enum.zip(list, list)
+      map = Map.new(zipped_list)
+
+      check all({range, insertion_point} <- rotation_spec(zipped_list)) do
+        rotation = &XUtil.Enum.rotate(&1, range, insertion_point)
+        assert rotation.(zipped_list) == rotation.(map)
+      end
+    end
+
+    test "on an empty enum produces an empty list" do
+      for enum <- [[], %{}, 0..-1//1, MapSet.new()] do
+        assert XUtil.Enum.rotate(enum, 0..0, 0) == []
+      end
+    end
+
+    test "on a list of a single element produces the same list" do
+      for single_element <- ["foo", 1, :foo, %{foo: "bar"}, MapSet.new(["foo"])] do
+        assert XUtil.Enum.rotate([single_element], 0..0, 0) == [single_element]
+      end
+    end
+
+    test "moves a single element" do
+      expected_numbers = Enum.flat_map([0..7, [14], 8..13, 15..20], &Enum.to_list/1)
+      assert XUtil.Enum.rotate(0..20, 14..14, 8) == expected_numbers
+
+      assert XUtil.Enum.rotate([:a, :b, :c, :d, :e, :f], 3..3, 2) == [:a, :b, :d, :c, :e, :f]
+    end
+
+    test "on a subsection of a list reorders the range correctly" do
+      expected_numbers = Enum.flat_map([0..7, 14..18, 8..13, 19..20], &Enum.to_list/1)
+      assert XUtil.Enum.rotate(0..20, 14..18, 8) == expected_numbers
+
+      assert XUtil.Enum.rotate([:a, :b, :c, :d, :e, :f], 3..4, 2) == [:a, :b, :d, :e, :c, :f]
+    end
+
+    property "handles negative indices" do
+      check all(
+              list <- StreamData.list_of(StreamData.integer(), max_length: 100),
+              {range, insertion_point} <- rotation_spec(list)
+            ) do
+        length = length(list)
+        negative_range = %Range{first: range.first - length, last: range.last - length, step: 1}
+
+        assert XUtil.Enum.rotate(list, negative_range, insertion_point) ==
+                 XUtil.Enum.rotate(list, range, insertion_point)
+      end
+    end
+
+    test "handles mixed positive and negative indices" do
+      assert XUtil.Enum.rotate(0..20, -6..-1, 8) == XUtil.Enum.rotate(0..20, 15..20, 8)
+      assert XUtil.Enum.rotate(0..20, 15..-1//1, 8) == XUtil.Enum.rotate(0..20, 15..20, 8)
+      assert XUtil.Enum.rotate(0..20, -6..20, 8) == XUtil.Enum.rotate(0..20, 15..20, 8)
+    end
+
+    test "raises an error when the step is not exactly 1" do
+      rotation_ranges_that_should_fail = [2..10//2, 8..-1, 10..2//-1, 10..4//-2, -1..-8//-1]
+
+      Enum.each(rotation_ranges_that_should_fail, fn range_that_should_fail ->
+        assert_raise(ArgumentError, fn ->
+          XUtil.Enum.rotate(0..20, range_that_should_fail, 1)
+        end)
+      end)
+    end
+
+    test "doesn't change the list when the first and middle indices match" do
+      assert XUtil.Enum.rotate(0..20, 8..18, 8) == Enum.to_list(0..20)
+      assert XUtil.Enum.rotate([:a, :b, :c, :d, :e, :f], 1..3, 1) == [:a, :b, :c, :d, :e, :f]
+    end
+
+    test "on the whole of a list reorders it correctly" do
+      expected_numbers = Enum.flat_map([10..20, 0..9], &Enum.to_list/1)
+      assert XUtil.Enum.rotate(0..20, 10..20, 0) == expected_numbers
+
+      assert XUtil.Enum.rotate([:a, :b, :c, :d, :e, :f], 4..5, 0) == [:e, :f, :a, :b, :c, :d]
+    end
+
+    test "raises when the insertion point is inside the range" do
+      assert_raise RuntimeError, fn ->
+        XUtil.Enum.rotate(0..20, 10..18, 14)
+      end
+    end
+
+    test "accepts range starts that are off the end of the list, returning the input list" do
+      assert XUtil.Enum.rotate([], 1..5, 0) == []
+      assert XUtil.Enum.rotate(0..20, 21..25, 3) == Enum.to_list(0..20)
+    end
+
+    test "accepts range ends that are off the end of the list, truncating the rotated range" do
+      assert XUtil.Enum.rotate(0..10, 8..15, 4) == XUtil.Enum.rotate(0..10, 8..10, 4)
+    end
+  end
+
+  # Generator for valid rotations on the input list
+  # Generates values of the form:
+  #   {range_to_rotate, insertion_point}
+  # ...such that the two arguments are always valid on the given list.
+  defp rotation_spec(list) do
+    max_idx = max(0, length(list) - 1)
+
+    StreamData.bind(StreamData.integer(0..max_idx), fn first ->
+      StreamData.bind(StreamData.integer(first..max_idx), fn last ->
+        allowable_insertion_points_at_end =
+          if last < max_idx do
+            [StreamData.integer((last + 1)..max_idx)]
+          else
+            []
+          end
+
+        allowable_insertion_points = [StreamData.integer(0..first)] ++ allowable_insertion_points_at_end
+
+        StreamData.bind(one_of(allowable_insertion_points), fn insertion_point ->
+          StreamData.constant({first..last, insertion_point})
+        end)
+      end)
+    end)
+  end
 end
